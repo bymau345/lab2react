459517ce5355af1c80bbf5fcbdd9d5ec81a479fa
israelMauricioCOndoriFuertes <condorimauricio6@gmail.com>
2021-09-14 00:57:57 -0400

    Avanzando en react

---
+++ src/apps/memory/MemoryComponents.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles.css";
+export const MemoryComponent = () => {
+    return <div>
+        <h1>Memory app</h1>
+        <hr />
+    </div>;
+};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import React from "react";
+import ReactDOM from "react-dom";
+import ToDo from "./components/ToDo";
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<MemoryComponents />, document.getElementById("root"));
